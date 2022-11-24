0ffc993ceaf3ed4483dc2c2a2424409e3eb314a7
Abiodun Emmanuel <abiodunemma0394@gmail.com>
2022-11-24 16:17:56 +0100

    fixed error page names

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Signin from "./pages/signin";
 import Signup from "./pages/singup";
 import Dashboard from "./pages/dashboard";
 import Emailverified from "./pages/emailverified";
-<<<<<<< HEAD
 import Signedup from "./pages/signedup";
-=======
 import Otp from "./pages/otp";
->>>>>>> 5748cc316ecbb42c6d6e6138a6ee8f18e8c72d76
 
 function App() {
   return (
@@ -15,10 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="signup" element={<Signup />} />
-<<<<<<< HEAD
-=======
         <Route path="otp" element={<Otp />} />
->>>>>>> 5748cc316ecbb42c6d6e6138a6ee8f18e8c72d76
         <Route path="emailverified" element={<Emailverified />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="signedup" element={<Signedup />} />
