f16db1dc5e8f15a2de729b65220e64513b8f0ff8
Odiri Ighogboja <odizeeplatform@gmail.com>
2022-11-24 15:08:04 +0100

    added otp component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import Signin from './pages/signin';
-import Signup from './pages/singup';
-import Dashboard from './pages/dashboard';
-import Emailverified from './pages/emailverified';
+import { Routes, Route } from "react-router-dom";
+import Signin from "./pages/signin";
+import Signup from "./pages/singup";
+import Dashboard from "./pages/dashboard";
+import Emailverified from "./pages/emailverified";
+import Otp from "./pages/otp";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="signup" element={<Signup />} />
-        <Route path="emailverified" element={<Emailverified />} />        
+        <Route path="otp" element={<Otp />} />
+        <Route path="emailverified" element={<Emailverified />} />
         <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </div>
