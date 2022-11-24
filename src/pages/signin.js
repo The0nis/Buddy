9e11c89562ddbf0922e9a74a7fb5f9fc6b4be19f
Abiodun Emmanuel <abiodunemma0394@gmail.com>
2022-11-24 14:13:17 +0100

    added signed up page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import Verifyemail from '../components/Verifyemail/Verifyemail'
-
+import React from "react";
+import Signedup from "../components/SignedUp/Signedup";
 
 const signin = () => {
   return (
-    <div><Verifyemail/></div>
+    <div>
+      <Signedup />
+    </div>
+  );
+};
 
-
-
-  )
-}
-
-export default signin
+export default signin;
