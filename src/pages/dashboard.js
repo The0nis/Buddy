6163717362bd48e1888d7f85e81ca9d87fb3fbf6
TheOnis <onisamuelmichael@gmail.com>
2022-11-28 15:14:36 +0100

    conflict resolved

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,25 +1,17 @@
 import RightSideBar from "../components/RightSideBar/Index";
-//import styles from "./Styles/dashboard.module.scss";
+import styles from "./Styles/dashboard.module.scss";
 
 const dashboard = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      {/* <SideNav />
-      <Main />
-      <RightSideBar /> */}
-        <RightSideBar />
-=======
-    <div className={styles.dashboard}>
-      {/* <SideNav /> */}
+   <div className={styles.dashboard}>
       <div className={styles.dashboard__main}>
         <div className={styles.main__top_nav}>{/* <DashboardTopNav /> */}</div>
         <div className={styles.main__content_area}>
-          <Main />
+          {/* <Main /> */}
           <RightSideBar />
         </div>
       </div>
->>>>>>> 488d34da8debb28d544bba30467e8d3802272001
+
     </div>
   );
 };
