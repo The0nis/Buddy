23c6a79af37a937b744961349b6349ae73530e98
Odiri Ighogboja <odizeeplatform@gmail.com>
2022-11-28 09:48:01 +0100

    refactored dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,10 +2,15 @@ import styles from "./Styles/dashboard.module.scss";
 
 const dashboard = () => {
   return (
-    <div>
-      {/* <SideNav />
-      <Main />
-      <RightSideBar /> */}
+    <div className={styles.dashboard}>
+      {/* <SideNav /> */}
+      <div className={styles.dashboard__main}>
+        <div className={styles.main__top_nav}>{/* <DashboardTopNav /> */}</div>
+        <div className={styles.main__content_area}>
+          <Main />
+          <RightSideBar />
+        </div>
+      </div>
     </div>
   );
 };
