cba12626bae552d1d5caf73b7f12c89ad5bd6b69
Abiodun Emmanuel <abiodunemma0394@gmail.com>
2022-11-28 08:22:20 +0100

    singupscreen added

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -2,7 +2,9 @@ import React from "react";
 import Signupscreen from "../components/Signupscreen/Index";
 
 const signin = () => {
-  return <div>Signupscreen</div>;
+  return <div>
+    <Signupscreen />
+  </div>;
 };
 
 export default signin;
