c365cbe87a82f888892ac8b0723253ce9eadc930
TheOnis <onisamuelmichael@gmail.com>
2022-11-30 17:30:42 +0100

    done

--- src/pages/signupscreen.js
+++ src/pages/signupscreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PageWrap from "../components/PageWrap/Index";
-import Signupscreen from "../components/Signupscreen/Index";
+import Signupscreen from "../components/Signupscreen/index";
 
 const signupscreen = () => {
   return (
